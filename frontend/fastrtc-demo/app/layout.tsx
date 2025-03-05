fa1d88ac95a752ed0ff55687ada98005649812d1
rohanprichard <rohanprichard@gmail.com>
2025-03-05 20:13:17 +0530

    adding transitions

--- frontend/fastrtc-demo/app/layout.tsx
+++ frontend/fastrtc-demo/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { ThemeTransition } from "@/components/ui/theme-transition";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <ThemeTransition />
         </ThemeProvider>
       </body>
     </html>
